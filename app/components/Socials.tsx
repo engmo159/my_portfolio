5579c5af14018c966567a1b3122522e5b640ae51
Mohammed <engmo159@git.com>
2024-07-03 12:54:02 +0300

    integrate whatsapp

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -4,10 +4,22 @@ import {
   RiGithubFill,
   RiLinkedinBoxFill,
 } from 'react-icons/ri'
+import { FaWhatsapp } from 'react-icons/fa'
 
 const Socials = () => {
+  const phoneNumber = '+201111030337'
+  const message = 'Hello, I have a question about your services!'
   return (
     <div className='flex items-center gap-x-5 text-lg lg:text-3xl'>
+      <Link
+        href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+          message
+        )}`}
+        className='hover:text-accent transition-all duration-300'
+        target='_blank'
+      >
+        <FaWhatsapp />
+      </Link>
       <Link
         href={'https://www.facebook.com/profile.php?id=100089993231546'}
         className='hover:text-accent transition-all duration-300'
